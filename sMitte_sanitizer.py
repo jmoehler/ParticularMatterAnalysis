10d1b32ff4fe63d0f604ce252f50fcdc9bf3f134
Jakob <jakob@Jakobs-MBP.fritz.box>
2019-05-01 22:44:12 +0200

    stuttgart sanitzer added

---
+++ sMitte_sanitizer.py
@@ -0,0 +1,43 @@
+import csv
+import time
+
+from datetime import datetime
+
+start = time.time()
+i=0
+o=0
+
+with open('./data/Stuttgart/clear_Halbstd-Werte-Stuttgart-Mitte-SZ_1987-2019(to30.4.).csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=';')
+
+    for datafile in ['./data/Stuttgart/S-Mitte-SZ-Halbstd-Werte_2019(bis 30.4.).csv', 
+        './data/Stuttgart/S-Mitte-SZ-Halbstd-Werte_2019(bis 30.4.).csv']:
+        with open(datafile,newline = '') as f:
+            readers = csv.reader(f, delimiter= ';')
+            for row in readers:
+                i += 1
+
+                if len(row) < 20:
+                    continue
+                
+                timeStr = row[0]
+                try:
+                    datetime.strptime(timeStr, '%m/%d/%y') 
+                except: 
+                    continue
+
+                # csv programm schreiben
+                o += 1
+                spamwriter.writerow(row)
+
+        f.close()
+
+
+ende = time.time()
+diff = ende-start
+
+print ("wrote %d (of %d) rows" % (o,i))
+print("runtime: %.5fs" %(diff)) 
+            
+
+
